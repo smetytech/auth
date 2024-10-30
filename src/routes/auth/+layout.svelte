b0ada4f2baa26c7a89261ecb318cff296478e917
musateduardbogdan <smetytech@gmail.com>
2024-10-30 16:04:49 +0200

    Update design

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -13,27 +13,44 @@
 	}
 </script>
 
-<div
-	class="mx-auto flex min-h-screen max-w-[80rem] grow flex-col justify-between gap-12 p-6 lg:p-10"
->
-	<div class="flex items-center justify-between">
-		<Button variant="ghost" onclick={handleBackButtonClick}>
-			<ArrowLeft class="!h-5 !w-5" />
-			<span>Back</span>
-		</Button>
+<div class="relative grid h-screen lg:grid-cols-2">
+	<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+		<div
+			class="absolute inset-0 bg-[url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)] bg-cover"
+		></div>
 
-		{#if isSignUpRoute}
-			<Button variant="ghost" href="/auth/sign-in">
-				<span>Sign In</span>
-			</Button>
-		{:else if isSignInRoute}
-			<Button variant="ghost" href="/auth/sign-up">
-				<span>Sign Up</span>
-			</Button>
-		{/if}
+		<span class="relative block text-lg font-medium">Smetytech.</span>
+		<div class="relative mt-auto">
+			<blockquote class="space-y-2">
+				<p>
+					&ldquo;This library has saved me countless hours of work and helped me deliver stunning
+					designs to my clients faster than ever before. Highly recommended!&rdquo;
+				</p>
+				<footer class="text-sm">Mihnea Huțuțui</footer>
+			</blockquote>
+		</div>
 	</div>
 
-	<div class="mx-auto w-full max-w-sm grow lg:flex lg:items-center">
-		{@render children()}
+	<div class="relative h-full p-4 lg:p-8">
+		<div class="absolute inset-x-4 top-4 flex items-center justify-between lg:inset-x-8 lg:top-8">
+			<Button variant="ghost" onclick={handleBackButtonClick}>
+				<ArrowLeft class="!h-5 !w-5" />
+				<span>Back</span>
+			</Button>
+
+			{#if isSignUpRoute}
+				<Button variant="ghost" href="/auth/sign-in">
+					<span>Sign In</span>
+				</Button>
+			{:else if isSignInRoute}
+				<Button variant="ghost" href="/auth/sign-up">
+					<span>Sign Up</span>
+				</Button>
+			{/if}
+		</div>
+
+		<div class="mx-auto flex h-full w-full max-w-96 items-center justify-center px-2 py-14 lg:px-0">
+			{@render children()}
+		</div>
 	</div>
 </div>
--- src/routes/auth/sign-up/+page.svelte
+++ src/routes/auth/sign-up/+page.svelte
@@ -110,4 +110,12 @@
 			<span>Continue</span>
 		</Button>
 	</form>
+
+	<p class="px-8 text-center text-sm text-muted-foreground">
+		By clicking continue, you agree to our
+		<a href="/terms" class="underline underline-offset-4 hover:text-primary">Terms of Service</a>
+		and
+		<a href="/privacy" class="underline underline-offset-4 hover:text-primary">Privacy Policy</a>
+		&#8203;.
+	</p>
 </div>
